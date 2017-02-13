7207a4bcfdc8c821d08ed1fdd0f2885f77b0b382
Diego Staphorst <diego-s-@hotmail.com>
2017-02-13 14:09:28 +0100

    create styling.css & javascript files & README.md

---
+++ week2/README.md
@@ -0,0 +1,4 @@
+# Web App From Scratch
+Course from the minor Web Development 1617 at the Hogeschool van Amsterdam. During this week of the course I'll learn to use API's. For this assignment I'll make use of the wunderground API and geonames API.
+ 
+ 
--- week2/static/app/app.js
+++ week2/static/app/app.js
@@ -1,3 +1,64 @@
 /**
  * Created by Diego on 13-02-17.
  */
+
+
+/**
+* Using API's
+ *  - GeoNames/(Google map API)
+ *  - Wunderground
+ *
+ *  With this this I'll ask the end-user to put a location in the input textfield. With this information input the api
+ *  wunderground is used for weather prediction.
+* */
+
+// Example filter, map and reduce
+// console.log(
+//     [1,2,3,4,5]
+//         .filter((el) => {return el < 5;})
+//         .map((el) => {return "<h1>" + el + "<h1>\r\n";})
+//         .reduce((buffer, el) => {return buffer + el;})
+// );
+
+
+const GEONAMES = "api.geonames.org/postalCodeSearch?";
+
+let postalcode = "2037pc";
+let country = "Nederland";
+
+const REQUEST = 'http://api.geonames.org/postalCodeLookupJSON?postalcode=' + postalcode  + '&country=' + country  + '&callback=getLocation&username=demo';
+
+let wtf = new JSONscriptRequest(REQUEST);
+console.log(wtf);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
