71d33df3a39d74bb33c3b96bbbae24413dfe3a64
Diego Staphorst <diego-s-@hotmail.com>
2017-02-15 13:59:01 +0100

    on menu click changes persons

--- week2/static/app/app.js
+++ week2/static/app/app.js
@@ -9,19 +9,28 @@
  *  With this this I'll ask the end-user to put a location in the input textfield. With this information input the api
  *  wunderground is used for weather prediction.
 * */
+// Example filter, map and reduce
+// console.log(
+//     [1,2,3,4,5]
+//         .filter((el) => {return el < 5;})
+//         .map((el) => {return "<h1>" + el + "<h1>\r\n";})
+//         .reduce((buffer, el) => {return buffer + el;})
+// );
+
+
 
 (function () {
-    var app = {
+    let app = {
         init: function() {
             routes.init();
         }
     };
 
-    var routes = {
+    let routes = {
         init: function () {
             routie({
                 'male_female': function() {
-                    sections.toggle("")
+                    sections.toggle(location.hash)
                 },
                 'male': function() {
                     sections.toggle(location.hash)
@@ -38,21 +47,44 @@
 
     }
 
-    function loadJSON(apiURL) {
-        let randomPeople = [];
+    function loadJSON(apiURL, hashURL) {
         fetch(apiURL)
             .then(data => data.json())
-            .then(data => randomPeople.push(...data.results));
-        return randomPeople;
+            .then(data => appendPeople(data.results, hashURL.replace('#', '')))
     }
 
-    function setElements(randomUsers) {
-        console.log(randomUsers);
+
+    function clearSection(section) {
+        while (section.firstChild) {
+            section.removeChild(section.firstChild);
+        }
     }
 
-    var sections = {
+    function hideSections(sectionID) {
+        articlesByGender = Array.from(document.getElementById("gender-groups").getElementsByTagName("article"));
+        articlesByGender.forEach(article => {
+            "use strict";
+            article.id == sectionID ? article.classList.remove("hide") : article.classList.add("hide");
+        })
+    }
+
+
+    function appendPeople(randomPeople, sectionID) {
+        const section = document.getElementById(sectionID);
+        clearSection(section);
+        hideSections(sectionID);
+        randomPeople.forEach(person => {
+            let imgNode = document.createElement("img");
+            imgNode.src = person.picture.large;
+            if (imgNode != null) {
+                section.appendChild(imgNode);
+            }
+        }
+    )};
+
+    let sections = {
         toggle: function (hashURL) {
-            setElements(loadJSON(createURL(hashURL)));
+            loadJSON(createURL(hashURL), hashURL);
         }
     };
     app.init();
